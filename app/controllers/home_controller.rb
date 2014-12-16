4f24720670d77e09ea778183e676590df15359ee
Ali Abou-Zahr <ali.a.zahr@gmail.com>
2014-12-15 16:45:18 -0800

    events showing on calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def show
     @events = Event.all
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @events_by_date = Event.all.group_by(&:date)
   end
 end
