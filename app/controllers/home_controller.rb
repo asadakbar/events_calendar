2ebe732b82501a9d3aa263ab33ea8f664a17b580
Ali Abou-Zahr <ali.a.zahr@gmail.com>
2014-12-22 15:58:10 -0800

    kids created for each day of events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def show
-    @events = Event.all
+    @events = Event.all.group_by(&:date)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @events_by_date = Event.all.group_by(&:date)
   end
